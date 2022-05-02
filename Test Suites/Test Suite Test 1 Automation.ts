ab8194e3c49b55a136dda4fb2eb905a47e73478b
hilmi <fuaddhilmi97@gmail.com>
2022-05-02 09:01:52 +0700

    first project commit Test 1

---
+++ Test Suites/Test Suite Test 1 Automation.ts
@@ -0,0 +1,21 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Test Suite Test 1 Automation</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>bc06679b-2143-45f0-a68f-429e2e9b805d</testSuiteGuid>
+   <testCaseLink>
+      <guid>68c3c873-d244-46dd-9afd-2be6f41441d3</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Test1</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+</TestSuiteEntity>
